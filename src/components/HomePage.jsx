81789a356d1c3f098a7fc7a5f3186059cb8ab0a8
JaydenPahukula <jayden.pahukula@gmail.com>
2025-03-29 23:14:38 -0600

    add stuff to stories

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,20 +3,22 @@ import PostCard from "./PostCard";
 export default function HomePage({ posts, stories }) {
   return (
     <>
-      <div className="flex p-3 pb-1 gap-3 overflow-x-auto no-scrollbar">
+      <div className="flex p-3 pb-0 gap-3 overflow-x-auto no-scrollbar">
         {stories?.stories?.map((story, i) => {
           return (
-            <div
-              key={i}
-              className="[background:linear-gradient(45deg,#28de52_15%,#0cc5ed_75%);] w-16 h-16 flex-shrink-0 rounded-full p-[2px]"
-            >
-              <div className="bg-white rounded-full h-full w-full p-[2px]">
-                <img
-                  src={"../../" + story.profileImgUrl}
-                  alt="Circle Image"
-                  className="rounded-full object-cover w-full h-full"
-                />
+            <div key={i} className="flex-shrink-0">
+              <div className="[background:linear-gradient(45deg,#28de52_15%,#0cc5ed_75%);] w-16 mb-1 h-16 rounded-full p-[2px]">
+                <div className="bg-white rounded-full h-full w-full p-[2px]">
+                  <img
+                    src={"../../" + story.profileImgUrl}
+                    alt="Circle Image"
+                    className="rounded-full object-cover w-full h-full"
+                  />
+                </div>
               </div>
+              <p className="text-[9px] text-center text-ellipsis w-16 overflow-hidden">
+                {story.username}
+              </p>
             </div>
           );
         })}
