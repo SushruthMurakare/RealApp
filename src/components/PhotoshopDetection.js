5737e9d8c8699b952b5662f3a6b9bb0009514607
JaydenPahukula <jayden.pahukula@gmail.com>
2025-03-30 01:12:18 -0600

    add key.json

--- src/components/PhotoshopDetection.js
+++ src/components/PhotoshopDetection.js
@@ -1,80 +1,82 @@
-import Groq from 'groq-sdk';
+import Groq from "groq-sdk";
+import key from "../../key.json";
 
 //Takes in a boolean value to determine if it should check for photoshop or educational content.
 // A return value of true means that the image is okay to be uploaded, a no means that it should be rejected
 
-const groq = new Groq();
+const groq = new Groq({ apiKey: key.key, dangerouslyAllowBrowser: true });
+console.log(key.key);
 export async function PhotoshopDetection(photoshop, imageUrl) {
   if (photoshop) {
     const chatCompletion = await groq.chat.completions.create({
-      "messages": [
+      messages: [
         {
-          "role": "user",
-          "content": [
+          role: "user",
+          content: [
             {
-              "type": "text",
-              "text": "Based on the image, please answer the following question: Is this image edited with Photoshop? Look for things that could be off such as body shape, lighting, shadows, etc. It is okay if only things such as contrast and exposure have been changed, but not if any bodies or personal images have been altered to look 'better'. Look for inconsistencies in shadows and in the image. Answer with a single word, either 'yes' or 'no' on whether the photo has been tampered with. Provide no additional response. "
+              type: "text",
+              text: "Based on the image, please answer the following question: Is this image edited with Photoshop? Look for things that could be off such as body shape, lighting, shadows, etc. It is okay if only things such as contrast and exposure have been changed, but not if any bodies or personal images have been altered to look 'better'. Look for inconsistencies in shadows and in the image. Answer with a single word, either 'yes' or 'no' on whether the photo has been tampered with. Provide no additional response. ",
             },
             {
-              "type": "image_url",
-              "image_url": {
-                "url": `${imageUrl}`
-              }
-            }
-          ]
-        }
+              type: "image_url",
+              image_url: {
+                url: `${imageUrl}`,
+              },
+            },
+          ],
+        },
       ],
-      "model": "llama-3.2-90b-vision-preview",
-      "temperature": 1,
-      "max_completion_tokens": 1024,
-      "top_p": 1,
-      "stream": false,
-      "stop": null
+      model: "llama-3.2-90b-vision-preview",
+      temperature: 1,
+      max_completion_tokens: 1024,
+      top_p: 1,
+      stream: false,
+      stop: null,
     });
-  
-     console.log(chatCompletion.choices[0].message.content);
-     if (chatCompletion.choices[0].message.content.toLowerCase().includes("yes")) {
+
+    console.log(chatCompletion.choices[0].message.content);
+    if (
+      chatCompletion.choices[0].message.content.toLowerCase().includes("yes")
+    ) {
       return false;
-     }
-     else {
+    } else {
       return true; // Return true or false based on the response
-     }
-      // Yes means that the image has been altered, no means that it is good
-  }
-  else {
+    }
+    // Yes means that the image has been altered, no means that it is good
+  } else {
     const chatCompletion = await groq.chat.completions.create({
-      "messages": [
+      messages: [
         {
-          "role": "user",
-          "content": [
+          role: "user",
+          content: [
             {
-              "type": "text",
-              "text": "Based on the image, please answer the following question: Is this image educational? Does it give some sort of information that could be useful to the viewer? Respond with a yes or a no. Nothing else, just a yes or no response. "
+              type: "text",
+              text: "Based on the image, please answer the following question: Is this image educational? Does it give some sort of information that could be useful to the viewer? Respond with a yes or a no. Nothing else, just a yes or no response. ",
             },
             {
-              "type": "image_url",
-              "image_url": {
-                "url": `${imageUrl}`
-              }
-            }
-          ]
-        }
+              type: "image_url",
+              image_url: {
+                url: `${imageUrl}`,
+              },
+            },
+          ],
+        },
       ],
-      "model": "llama-3.2-90b-vision-preview",
-      "temperature": 1,
-      "max_completion_tokens": 1024,
-      "top_p": 1,
-      "stream": false,
-      "stop": null
+      model: "llama-3.2-90b-vision-preview",
+      temperature: 1,
+      max_completion_tokens: 1024,
+      top_p: 1,
+      stream: false,
+      stop: null,
     });
-  
-     console.log(chatCompletion.choices[0].message.content);
-     if (chatCompletion.choices[0].message.content.toLowerCase().includes("yes")) {
+
+    console.log(chatCompletion.choices[0].message.content);
+    if (
+      chatCompletion.choices[0].message.content.toLowerCase().includes("yes")
+    ) {
       return true;
-     }
-     else {
+    } else {
       return false; // Return true or false based on the response
-     }
+    }
   }
-  
 }
